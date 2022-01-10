0b950178f49b8b74145da84f94dafeda0686e246
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-10 10:04:24 -0300

    primeiro commit

---
+++ src/test/java/com/bookstorespring/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstorespring.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
