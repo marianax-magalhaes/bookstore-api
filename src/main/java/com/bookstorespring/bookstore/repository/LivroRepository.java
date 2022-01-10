5118dbcf984bd62d42522c4aaa7d1080254d12a5
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-10 11:05:18 -0300

    repositorio livro

---
+++ src/main/java/com/bookstorespring/bookstore/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.bookstorespring.bookstore.repository;
+
+import com.bookstorespring.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
